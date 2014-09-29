7290576795a822278025f064f6dfff46381b964f
SAMEH <sameh.bellez@gmail.com>
2014-09-29 14:34:45 +0100

    Error handling

--- src/main/java/com/goeuro/main/GoEuroTest.java
+++ src/main/java/com/goeuro/main/GoEuroTest.java
@@ -9,8 +9,10 @@ import com.goeuro.model.Location;
 import com.goeuro.utils.CsvWriter;
 import com.goeuro.utils.RestConsumer;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.web.client.ResourceAccessException;
 
 /**
  *
@@ -23,19 +25,21 @@ public class GoEuroTest {
     public static void main(String[] args) {
 
         if (args.length == 1) {
-            RestConsumer<Location[]> consumer = new RestConsumer(Location[].class);
-            System.out.println("sending the http request to the distant API...");
-            Location[] list = consumer.getDataAsObjects(BASE_URL + args[0]);
-            System.out.println("Generating the csv file...");
-            CsvWriter csvWriter = new CsvWriter();
             try {
+                RestConsumer<Location[]> consumer = new RestConsumer(Location[].class);
+                System.out.println("sending the http request to the distant API...");
+                Location[] list = consumer.getDataAsObjects(BASE_URL + args[0]);
+                System.out.println("Generating the csv file...");
+                CsvWriter csvWriter = new CsvWriter();
                 csvWriter.writeData(list);
+
+            } catch (ResourceAccessException e) {
+                System.out.println("Error while connecting to the distant API");
             } catch (IOException ex) {
                 System.out.println("Error while writing the csv file");
             }
 
-        }
-        else {
+        } else {
             System.out.println("Missing user entry");
         }
     }
